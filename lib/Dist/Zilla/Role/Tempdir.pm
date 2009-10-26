3f67745d825d96b670c05bf6a9cb448aedac3a24
Kent Fredric <kentfredric@gmail.com>
2009-10-26 15:37:51 +1300

    working product

--- lib/Dist/Zilla/Role/Tempdir.pm
+++ lib/Dist/Zilla/Role/Tempdir.pm
@@ -1,10 +1,11 @@
-package Dist::Zilla::Role::Tempdir;
-
-# ABSTRACT: Shell Out and collect the result in a DZ plugin.
-
-# $Id:$
 use strict;
 use warnings;
+
+package Dist::Zilla::Role::Tempdir;
+
+# ABSTRACT: Shell Out and collect the result in a DZ plug-in.
+
+# $Id:$
 
 use Moose::Role;
 use Digest::SHA;
@@ -12,6 +13,8 @@ use File::Tempdir;
 use File::Slurp qw( write_file read_file );
 use Path::Class qw( dir file );
 use File::chdir;
+use File::Find::Rule;
+use Dist::Zilla::File::InMemory;
 
 use namespace::autoclean;
 
@@ -29,16 +32,15 @@ use namespace::autoclean;
 
     my ( @generated_files ) = $self->capture_tempdir(sub{
       system( $somecommand );
-    },{
-      ignore_updated => 1,
     });
 
     for ( @generated_files ) {
-      if( $_->name =~ qr/someregex/ ){
-        $self->add_file( $file );
+      if( $_->{status} eq 'N' && $_->{name} =~ qr/someregex/ ){
+        $self->add_file( $_->{file});
       }
     }
   }
+
 =cut
 
 =head1 METHODS
@@ -47,26 +49,20 @@ use namespace::autoclean;
 
 Creates a File::Tempdir and dumps the current state of Dist::Zilla's files into it.
 
-Runs the specified codesub CHDir'ed into that tmpdir, and captures the changed files.
+Runs the specified code sub C<chdir>'ed into that C<tmpdir>, and captures the changed files.
+
+  my ( @array ) = $self->capture_tempdir(sub{
+
+  });
+
+Response is an array of hash-ref.
+
+    { name => 'file/Name/Here' ,
+      status => 'O' # O = Original, N = New, M = Modified, D = Deleted
+      file   => Dist::Zilla::Role::File object ( missing if status => 'D'.
+    }
 
 =cut
-
-has _digester => (
-  isa        => 'Digest::base',
-  is         => 'rw',
-  lazy_build => 1,
-);
-
-sub _build__digester {
-  return Digest::SHA->new(512);
-}
-
-sub _digest_for {
-  my ( $self, $data ) = @_;
-  $self->_digester->reset;
-  $self->_digester->add($$data);
-  return $self->_digester->b64digest;
-}
 
 sub capture_tempdir {
   my ( $self, $code, $args ) = @_;
@@ -81,28 +77,124 @@ sub capture_tempdir {
 
   my ( $tempdir, $dir );
   $tempdir = File::Tempdir->new();
-  $dir     = $tempdir->name;
+  $dir     = dir( $tempdir->name );
 
   my %input_files;
 
   for my $file ( @{ $dzil->files } ) {
     my ( $name, $content, ) = ( $file->name, $file->content, );
 
-    $input_files{ $file->name } = { hash => $self->_digest_for( \$content ), };
+    $input_files{ $file->name } = {
+      hash => $self->_digest_for( \$content ),
+      file => $file,
+    };
 
-    my ($tmpfile) = dir($dir)->file($name);
+    my ($tmpfile) = $dir->file($name);
     $tmpfile->dir->mkpath(1);
-    write_file( $tmpfile->absolute . "", \$content );
+    write_file( $tmpfile->absolute->stringify, \$content );
   }
-  use Data::Dump qw( dump );
-  dump \%input_files;
   {
+    ## no critic ( ProhibitLocalVars )
     local $CWD = $dir;
     $code->();
   }
+  my (@files) = File::Find::Rule->file->in($dir);
 
+  my %output_files;
 
+  for ( keys %input_files ) {
+    $output_files{$_} = {
+      status => 'D',
+      name   => $_,
+      file   => $input_files{$_}->{file},
+    };
+  }
+
+  for my $filename (@files) {
+
+    my $shortname = file($filename)->relative($dir)->stringify;
+    my $content   = file($filename)->slurp;
+    my $hash      = $self->_digest_for( \$content );
+
+    if ( exists $input_files{$shortname} ) {
+
+      # FILE NOT MODIFIED, (O)riginal
+
+      if ( $input_files{$shortname}->{hash} eq $hash ) {
+        $output_files{$shortname}->{status} = 'O';
+        $output_files{$shortname}->{file}   = $input_files{$shortname}->{file};
+        next;
+      }
+
+      # FILE (M)odified
+      $output_files{$shortname}->{status} = 'M';
+      $output_files{$shortname}->{file}   = Dist::Zilla::File::InMemory->new(
+        name    => $shortname,
+        content => $content,
+      );
+      next;
+    }
+
+    # FILE (N)ew
+    $output_files{$shortname} = {
+      status => 'N',
+      name   => $shortname,
+      file   => Dist::Zilla::File::InMemory->new(
+        name    => $shortname,
+        content => $content,
+      ),
+    };
+  }
+
+  return values %output_files;
 }
 
+=head1 PRIVATE ATTRIBUTES
+
+=head2 _digester
+
+  isa => Digest::base,
+  is  => rw,
+  lazy_build => 1
+
+Used for Digesting the contents of files.
+
+=cut
+
+has _digester => (
+  isa        => 'Digest::base',
+  is         => 'rw',
+  lazy_build => 1,
+);
+
+=head1 PRIVATE METHODS
+
+=head2 _build__digester
+
+returns an instance of Digest::SHA with 512bit hashes.
+
+=cut
+
+sub _build__digester {
+  ## no critic ( ProhibitMagicNumbers )
+  return Digest::SHA->new(512);
+}
+
+=head2 _digest_for
+
+  my $hash = $self->_digest_for( \$content );
+
+Hashes content and returns the result in b64.
+
+=cut
+
+sub _digest_for {
+  my ( $self, $data ) = @_;
+  $self->_digester->reset;
+  $self->_digester->add( ${$data} );
+  return $self->_digester->b64digest;
+}
+
+no Moose::Role;
 1;
 
