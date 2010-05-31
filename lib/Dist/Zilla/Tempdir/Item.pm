b155b69f8781d4e0346528e83fb7d53c3cb0277b
Kent Fredric <kentfredric@gmail.com>
2010-05-31 13:54:59 +1200

    Documenting the ::Item module

--- lib/Dist/Zilla/Tempdir/Item.pm
+++ lib/Dist/Zilla/Tempdir/Item.pm
@@ -11,17 +11,79 @@ use Scalar::Util qw( blessed );
 
 use namespace::autoclean;
 
+=head1 SYNOPSIS
+
+  my $foo = Dist::Zilla::Tempdir::Item->new( 
+    name => 'Path/To/File.txt',
+    file => $dzilfile,
+  );
+  $foo->set_new;
+  $foo->is_new; # true
+  $foo->is_deleted; # false
+  $foo->set_deleted;
+  $foo->is_new; # false
+  $foo->is_deleted; # true.
+
+Ultimately, I figured using a Character with "eq" every where in userland
+was a way to extra bugs that were hard to detect. Going via all the OO niceness 
+you'll probably incur* a small performance penalty,  but things going B<Bang> when you
+make a typo or add invisible whitespace is a GoodThing.
+
+* albeit infintesimally insignificant in size, especially for something that will only take 15 seconds of run-time every once in a while, not to mention the overhead is drowned by the fact we're doing filesystem IO and running many of the files through a complete hashing algorithm to test for modification.
+
+=cut
+
+=head1 ATTRIBUTES
+
+=head2 status
+
+  isa => Str, 
+  is  => rw,
+
+The internal status character. You can mangle this yourself if you want, and for compatibility with older versions
+of this dist, you may even have to, but try not to, if it breaks, something something something pieces.
+
+Using the is_* and set_* accessors is a I<much> smarter idea.
+
+At present, the characters M, O, N and D have defined meanings, but this could change. ( Its not even unforseeable expanding it to 
+be 2 characters to represent different parts of state, I probably will not do that, but do not pretend I will not ;] )
+
+=cut
+
 has 'status' => ( 
   isa        => 'Str',
 #  required   => 1,
+# TODO: use MooseX::LazyRequire -- kentnl 2010-05-31
   is         => 'rw',
 );
+
+=head2 file
+
+  isa      => Dist::Zilla::Role::File,
+  required => 1, 
+  is       => rw
+
+This is the Dist::Zilla::File::* item which we refer to. For items that C<is_deleted>, C<file> is likely to be the file before it got deleted.
+
+For C<is_new> and C<is_original> files, the item is the file itself, and for C<is_modified>, its the modified version of the file.
+
+=cut
 
 has 'file' => (
   isa      => 'Dist::Zilla::Role::File',
   required => 1,
   is       => 'rw',
 );
+
+=head2 name
+
+  isa      => Str, 
+  required => 1,
+  is       => rw,
+
+This is the path to the file relative to the dist root. 
+
+=cut
 
 has 'name' => (
   isa      => 'Str',
@@ -58,11 +120,56 @@ sub _mk_status {
   }
 }
 
-_mk_status('modified', 'M');
-_mk_status('original', 'O');
-_mk_status('new',      'N' );
-_mk_status('deleted',  'D' );
+=head1 METHODS
 
+=head2 is_modified
+
+returns if the file is modified or not.
+
+=head2 set_modified
+
+sets the state to 'modified'
+
+=cut
+
+_mk_status('modified', 'M');
+
+=head2 is_original
+
+returns if the file is the original file or not.
+
+=head2 set_original
+
+sets the state to 'original'
+
+=cut
+
+_mk_status('original', 'O');
+
+=head2 is_new
+
+returns if the file is new or not ( that is, if it wasn't in the dist prior to executing 
+the given code ).
+
+=head2 set_new
+
+sets the state to 'new'
+
+=cut
+
+_mk_status('new',      'N' );
+
+=head2 is_deleted
+
+returns if the file is deleted or not ( that is, if it were deleted during the execution phase )
+
+=head2 set_deleted
+
+sets the state to 'deleted'
+
+=cut
+
+_mk_status('deleted',  'D' );
 
 __PACKAGE__->meta->make_immutable;
 
