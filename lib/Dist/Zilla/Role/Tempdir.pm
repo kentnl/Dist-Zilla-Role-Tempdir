374953e9793a07edc06ac2ce6e21e6f1ef75392c
Kent Fredric <kentfredric@gmail.com>
2010-07-24 09:14:36 +1200

    Build results of b8ad747 (on master)

--- lib/Dist/Zilla/Role/Tempdir.pm
+++ lib/Dist/Zilla/Role/Tempdir.pm
@@ -2,6 +2,9 @@ use strict;
 use warnings;
 
 package Dist::Zilla::Role::Tempdir;
+BEGIN {
+  $Dist::Zilla::Role::Tempdir::VERSION = '0.01027622';
+}
 
 # ABSTRACT: Shell Out and collect the result in a DZ plug-in.
 
@@ -17,65 +20,7 @@ use Dist::Zilla::Tempdir::Item;
 
 use namespace::autoclean;
 
-=head1 SYNOPSIS
 
-  package #
-    Dist::Zilla::Plugin::FooBar;
-
-  use Moose;
-  with 'Dist::Zilla::Role::Tempdir';
-  with 'Dist::Zilla::Role::FileInjector';
-  with 'Dist::Zilla::Role::InstallTool';
-
-  sub setup_installer {
-    my ( $self, $arg ) = @_ ;
-
-    my ( @generated_files ) = $self->capture_tempdir(sub{
-      system( $somecommand );
-    });
-
-    for ( @generated_files ) {
-      if( $_->is_new && $_->name =~ qr/someregex/ ){
-        $self->add_file( $_->file );
-      }
-    }
-  }
-
-This role is a convenience role for factoring into other plug-ins to use the power of Unix
-in any plug-in.
-
-If for whatever reason you need to shell out and run your own app that is not Perl ( i.e.: Java )
-to go through the code and make modifications, produce documentation, etc, then this role is for you.
-
-Important to note however, this role B<ONLY> deals with getting Dist::Zilla's state written out to disk,
-executing your given arbitrary code, and then collecting the results. At no point does it attempt to re-inject
-those changes back into L<Dist::Zilla>. That is left as an exercise to the plug-in developer.
-
-=cut
-
-=head1 METHODS
-
-=head2 capture_tempdir
-
-Creates a File::Tempdir and dumps the current state of Dist::Zilla's files into it.
-
-Runs the specified code sub C<chdir>'ed into that C<tmpdir>, and captures the changed files.
-
-  my ( @array ) = $self->capture_tempdir(sub{
-
-  });
-
-Response is an array of L<Dist::Zilla::Tempdir::Item>
-
-   [ bless( { name => 'file/Name/Here' ,
-      status => 'O' # O = Original, N = New, M = Modified, D = Deleted
-      file   => Dist::Zilla::Role::File object
-    }, 'Dist::Zilla::Tempdir::Item' ) , bless ( ... ) ..... ]
-
-Make sure to look at L<Dist::Zilla::Tempdir::Item> for usage.
-
-
-=cut
 
 sub capture_tempdir {
   my ( $self, $code, $args ) = @_;
@@ -164,6 +109,98 @@ sub capture_tempdir {
   return values %output_files;
 }
 
+
+has _digester => (
+  isa        => 'Digest::base',
+  is         => 'rw',
+  lazy_build => 1,
+);
+
+
+sub _build__digester {
+  ## no critic ( ProhibitMagicNumbers )
+  return Digest::SHA->new(512);
+}
+
+
+sub _digest_for {
+  my ( $self, $data ) = @_;
+  $self->_digester->reset;
+  $self->_digester->add( ${$data} );
+  return $self->_digester->b64digest;
+}
+
+
+no Moose::Role;
+1;
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::Role::Tempdir - Shell Out and collect the result in a DZ plug-in.
+
+=head1 VERSION
+
+version 0.01027622
+
+=head1 SYNOPSIS
+
+  package #
+    Dist::Zilla::Plugin::FooBar;
+
+  use Moose;
+  with 'Dist::Zilla::Role::Tempdir';
+  with 'Dist::Zilla::Role::FileInjector';
+  with 'Dist::Zilla::Role::InstallTool';
+
+  sub setup_installer {
+    my ( $self, $arg ) = @_ ;
+
+    my ( @generated_files ) = $self->capture_tempdir(sub{
+      system( $somecommand );
+    });
+
+    for ( @generated_files ) {
+      if( $_->is_new && $_->name =~ qr/someregex/ ){
+        $self->add_file( $_->file );
+      }
+    }
+  }
+
+This role is a convenience role for factoring into other plug-ins to use the power of Unix
+in any plug-in.
+
+If for whatever reason you need to shell out and run your own app that is not Perl ( i.e.: Java )
+to go through the code and make modifications, produce documentation, etc, then this role is for you.
+
+Important to note however, this role B<ONLY> deals with getting Dist::Zilla's state written out to disk,
+executing your given arbitrary code, and then collecting the results. At no point does it attempt to re-inject
+those changes back into L<Dist::Zilla>. That is left as an exercise to the plug-in developer.
+
+=head1 METHODS
+
+=head2 capture_tempdir
+
+Creates a File::Tempdir and dumps the current state of Dist::Zilla's files into it.
+
+Runs the specified code sub C<chdir>'ed into that C<tmpdir>, and captures the changed files.
+
+  my ( @array ) = $self->capture_tempdir(sub{
+
+  });
+
+Response is an array of L<Dist::Zilla::Tempdir::Item>
+
+   [ bless( { name => 'file/Name/Here' ,
+      status => 'O' # O = Original, N = New, M = Modified, D = Deleted
+      file   => Dist::Zilla::Role::File object
+    }, 'Dist::Zilla::Tempdir::Item' ) , bless ( ... ) ..... ]
+
+Make sure to look at L<Dist::Zilla::Tempdir::Item> for usage.
+
 =head1 PRIVATE ATTRIBUTES
 
 =head2 _digester
@@ -174,26 +211,11 @@ sub capture_tempdir {
 
 Used for Digesting the contents of files.
 
-=cut
-
-has _digester => (
-  isa        => 'Digest::base',
-  is         => 'rw',
-  lazy_build => 1,
-);
-
 =head1 PRIVATE METHODS
 
 =head2 _build__digester
 
 returns an instance of Digest::SHA with 512bit hashes.
-
-=cut
-
-sub _build__digester {
-  ## no critic ( ProhibitMagicNumbers )
-  return Digest::SHA->new(512);
-}
 
 =head2 _digest_for
 
@@ -201,21 +223,20 @@ sub _build__digester {
 
 Hashes content and returns the result in b64.
 
-=cut
-
-sub _digest_for {
-  my ( $self, $data ) = @_;
-  $self->_digester->reset;
-  $self->_digester->add( ${$data} );
-  return $self->_digester->b64digest;
-}
-
 =head1 SEE ALSO
 
 L<Dist::Zilla::Tempdir::Item>
 
-=cut
+=head1 AUTHOR
 
-no Moose::Role;
-1;
+Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Kent Fredric.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
 
--- lib/Dist/Zilla/Tempdir/Item.pm
+++ lib/Dist/Zilla/Tempdir/Item.pm
@@ -2,6 +2,9 @@ use strict;
 use warnings;
 
 package Dist::Zilla::Tempdir::Item;
+BEGIN {
+  $Dist::Zilla::Tempdir::Item::VERSION = '0.01027622';
+}
 
 # ABSTRACT: A result object for things that DO() DZ::R::Tempdir;
 
@@ -11,47 +14,7 @@ use Scalar::Util qw( blessed );
 
 use namespace::autoclean;
 
-=head1 SYNOPSIS
 
-  my $foo = Dist::Zilla::Tempdir::Item->new(
-    name => 'Path/To/File.txt',
-    file => $dzilfile,
-  );
-  $foo->set_new;
-  $foo->is_new; # true
-  $foo->is_deleted; # false
-  $foo->set_deleted;
-  $foo->is_new; # false
-  $foo->is_deleted; # true.
-
-Ultimately, I figured using a character with "C<eq>" every where in extending code
-was a way to extra bugs that were hard to detect. Going via all the Object-Oriented niceness
-you'll probably incur* a small performance penalty,  but things going B<Bang> when you
-make a typo or add invisible white-space is a Good Thing.
-
-* albeit immeasurably insignificant in size, especially for something that will only take
-15 seconds of run-time every once in a while, not to mention the overhead is drowned by the
-fact we're doing file-system IO and running many of the files through a complete hashing
-algorithm to test for modification.
-
-=cut
-
-=head1 ATTRIBUTES
-
-=head2 status
-
-  isa => Str,
-  is  => rw,
-
-The internal status character. You can mangle this yourself if you want, and for compatibility with older versions
-of this dist, you may even have to, but try not to, if it breaks, something something something pieces.
-
-Using the is_* and set_* accessors is a I<much> smarter idea.
-
-At present, the characters M, O, N and D have defined meanings, but this could change. ( Its not even unforeseeable expanding it to
-be 2 characters to represent different parts of state, I probably will not do that, but do not pretend I will not ;] )
-
-=cut
 
 has 'status' => (
   isa => 'Str',
@@ -61,17 +24,6 @@ has 'status' => (
   is => 'rw',
 );
 
-=head2 file
-
-  isa      => Dist::Zilla::Role::File,
-  required => 1,
-  is       => rw
-
-This is the Dist::Zilla::File::* item which we refer to. For items that C<is_deleted>, C<file> is likely to be the file before it got deleted.
-
-For C<is_new> and C<is_original> files, the item is the file itself, and for C<is_modified>, its the modified version of the file.
-
-=cut
 
 has 'file' => (
   isa      => 'Dist::Zilla::Role::File',
@@ -79,15 +31,6 @@ has 'file' => (
   is       => 'rw',
 );
 
-=head2 name
-
-  isa      => Str,
-  required => 1,
-  is       => rw,
-
-This is the path to the file relative to the dist root.
-
-=cut
 
 has 'name' => (
   isa      => 'Str',
@@ -126,6 +69,91 @@ sub _mk_status {
   return 1;
 }
 
+
+_mk_status( 'modified', 'M' );
+
+
+_mk_status( 'original', 'O' );
+
+
+_mk_status( 'new', 'N' );
+
+
+_mk_status( 'deleted', 'D' );
+
+__PACKAGE__->meta->make_immutable;
+
+no Moose;
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::Tempdir::Item - A result object for things that DO() DZ::R::Tempdir;
+
+=head1 VERSION
+
+version 0.01027622
+
+=head1 SYNOPSIS
+
+  my $foo = Dist::Zilla::Tempdir::Item->new(
+    name => 'Path/To/File.txt',
+    file => $dzilfile,
+  );
+  $foo->set_new;
+  $foo->is_new; # true
+  $foo->is_deleted; # false
+  $foo->set_deleted;
+  $foo->is_new; # false
+  $foo->is_deleted; # true.
+
+Ultimately, I figured using a character with "C<eq>" every where in extending code
+was a way to extra bugs that were hard to detect. Going via all the Object-Oriented niceness
+you'll probably incur* a small performance penalty,  but things going B<Bang> when you
+make a typo or add invisible white-space is a Good Thing.
+
+* albeit immeasurably insignificant in size, especially for something that will only take
+15 seconds of run-time every once in a while, not to mention the overhead is drowned by the
+fact we're doing file-system IO and running many of the files through a complete hashing
+algorithm to test for modification.
+
+=head1 ATTRIBUTES
+
+=head2 status
+
+  isa => Str,
+  is  => rw,
+
+The internal status character. You can mangle this yourself if you want, and for compatibility with older versions
+of this dist, you may even have to, but try not to, if it breaks, something something something pieces.
+
+Using the is_* and set_* accessors is a I<much> smarter idea.
+
+At present, the characters M, O, N and D have defined meanings, but this could change. ( Its not even unforeseeable expanding it to
+be 2 characters to represent different parts of state, I probably will not do that, but do not pretend I will not ;] )
+
+=head2 file
+
+  isa      => Dist::Zilla::Role::File,
+  required => 1,
+  is       => rw
+
+This is the Dist::Zilla::File::* item which we refer to. For items that C<is_deleted>, C<file> is likely to be the file before it got deleted.
+
+For C<is_new> and C<is_original> files, the item is the file itself, and for C<is_modified>, its the modified version of the file.
+
+=head2 name
+
+  isa      => Str,
+  required => 1,
+  is       => rw,
+
+This is the path to the file relative to the dist root.
+
 =head1 METHODS
 
 =head2 is_modified
@@ -136,10 +164,6 @@ returns if the file is modified or not.
 
 sets the state to 'modified'
 
-=cut
-
-_mk_status( 'modified', 'M' );
-
 =head2 is_original
 
 returns if the file is the original file or not.
@@ -147,10 +171,6 @@ returns if the file is the original file or not.
 =head2 set_original
 
 sets the state to 'original'
-
-=cut
-
-_mk_status( 'original', 'O' );
 
 =head2 is_new
 
@@ -161,10 +181,6 @@ the given code ).
 
 sets the state to 'new'
 
-=cut
-
-_mk_status( 'new', 'N' );
-
 =head2 is_deleted
 
 returns if the file is deleted or not ( that is, if it were deleted during the execution phase )
@@ -173,12 +189,16 @@ returns if the file is deleted or not ( that is, if it were deleted during the e
 
 sets the state to 'deleted'
 
+=head1 AUTHOR
+
+Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Kent Fredric.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
 =cut
 
-_mk_status( 'deleted', 'D' );
-
-__PACKAGE__->meta->make_immutable;
-
-no Moose;
-
-1;
