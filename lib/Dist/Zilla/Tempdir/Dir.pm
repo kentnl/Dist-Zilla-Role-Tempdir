3fffa9dfc2374efeb76ac3b63cb651352fa84c4a
Kent Fredric <kentfredric@gmail.com>
2014-07-29 20:34:57 +1200

    Build results of 210fbee (on master)

--- lib/Dist/Zilla/Tempdir/Dir.pm
+++ lib/Dist/Zilla/Tempdir/Dir.pm
@@ -48,7 +48,7 @@ sub _build__tempdir {
   my $template = 'DZ_R_Tempdir_';
   if ( $self->_has_tempdir_owner ) {
     my $owner = $self->_tempdir_owner;
-    $owner =~ s/[^A-Za-z0-9]+/_/g;
+    $owner =~ s/[^A-Za-z0-9]+/_/xmsg;
     $template .= $owner . '_';
   }
   $template .= 'XXXXXX';
